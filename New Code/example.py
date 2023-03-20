eafd397f3ffd37aaed502c1e6ffce148b7878df1
ssant96 <ssant96@vt.edu>
2023-03-20 13:53:17 -0400

    cleaned up files in New Code

--- New Code/example.py
+++
@@ -1,9 +0,0 @@
-dict = {
-    "a" : 2,
-    "b" : 7,
-    "c" : 9,
-    "d" : 10
-}
-
-for k in dict.keys():
-    print(k, dict[k])
--- New Code/placeholderData.py
+++
@@ -1,7 +0,0 @@
-# This script will convert the linuxcnc names to the xml names so that it translates to the agent. IS this supposed to be a parser?
-
-
-# Absolute X Position
-# Xabs = output
-
-
